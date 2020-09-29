1a94f0bada7fd631412f58c5430ceff3cf0bbf23
Maksim Bashchuk <MaksimBashchuk@gmail.com>
2020-09-29 11:36:08 +0300

    feat: add extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,79 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (options.repeatTimes === undefined) options.repeatTimes = 1;
+  if (options.additionRepeatTimes === undefined) options.additionRepeatTimes = 1;
+  if (options.addition === undefined) {
+    options.addition = '';
+  } else {
+    options.addition = String(options.addition);
+  }
+  if (options.separator === undefined) options.separator = '+';
+  if (options.additionSeparator === undefined) options.additionSeparator = '|';
+
+  str = String(str);
+  options.addition = options.addition.toString();
+  let additionRepeated = [];
+
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    additionRepeated.push(options.addition);
+  }
+
+  for (let i = 0; i < additionRepeated.length - 1; i++) {
+    additionRepeated[i] = `${additionRepeated[i]}${options.additionSeparator}`;
+  }
+
+  additionRepeated = additionRepeated.join('');
+  let strRepeated = [];
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    strRepeated.push(str + additionRepeated);
+  }
+
+  for (let i = 0; i < strRepeated.length - 1; i++) {
+    strRepeated[i] = `${strRepeated[i]}${options.separator}`;
+  }
+
+  return strRepeated.join('');
+
 };
-  
+ 
+
+
+
+// function repeater(str, options) {
+//   if (options.repeatTimes === undefined) options.repeatTimes = 1;
+//   if (options.additionRepeatTimes === undefined) options.additionRepeatTimes = 1;
+//   if (options.addition === undefined) options.addition = '';
+//   if (options.separator === undefined) options.separator = '+';
+//   if (options.additionSeparator === undefined) options.additionSeparator = '|';
+
+//   str = str.toString();
+//   options.addition = options.addition.toString();
+//   let additionRepeated = [];
+
+//   for (let i = 0; i < options.additionRepeatTimes; i++) {
+//     additionRepeated.push(options.addition);
+//   }
+
+//   for (let i = 0; i < additionRepeated.length - 1; i++) {
+//     additionRepeated[i] = `${additionRepeated[i]}${options.additionSeparator}`;
+//   }
+
+//   additionRepeated = additionRepeated.join('');
+//   let strRepeated = [];
+
+//   for (let i = 0; i < options.repeatTimes; i++) {
+//     strRepeated.push(str + additionRepeated);
+//   }
+
+//   for (let i = 0; i < strRepeated.length - 1; i++) {
+//     strRepeated[i] = `${strRepeated[i]}${options.separator}`;
+//   }
+
+//   return strRepeated.join('');
+// }
+
+// // console.log(repeater());
+// console.log(String(new Set()));
+// console.log(typeof(String(new Set())));
