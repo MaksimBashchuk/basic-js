3dc73832c989f2a67080575511f7da023fee2e80
Maksim Bashchuk <MaksimBashchuk@gmail.com>
2020-09-30 18:03:57 +0300

    init: start simple chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,58 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+
+  deleteChain() {
+    delete this.chain;
+  },
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    value = String(value);
+    if (this.chain === undefined) {
+      this.chain = `( ${value} )`;
+      return this;
+    } else {
+      this.chain = `${this.chain}~~( ${value} )~~`;
+      this.chain = this.chain.slice(0, -2);
+      return this;
+    }
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position < 1 || position > this.chain.split('~~').length) {
+      this.deleteChain();
+      throw new Error('THROWN');
+    } else {
+      this.chain = this.chain.split('~~').splice(position - 1, 1).join('~~');
+      return this;
+    }
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.split('~~');
+    this.chain =  this.chain.reverse().join('~~');
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain;
   }
 };
 
 module.exports = chainMaker;
+
+// 
+// console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain()
+// .reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain());
+
+
+
+// let str = 'asdweqwe~~asdqwe~~asde';
+// str = str.split('~~');
+// str.splice(1,1);
+// str = str.join('~~');
+// console.log(str);
