1b31cef1de471670f2452014154fc4becc07ae40
Maksim Bashchuk <MaksimBashchuk@gmail.com>
2020-10-01 17:46:37 +0300

    feat: start transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+// module.exports = 
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('THROWN');
+  let newArr = arr.map((item, i) => {
+    if (i === 0 && (item ==='--discard-prev' || item === '--double-prev')) {
+      return undefined;
+    } else if ((i === arr.length -1) && (item ==='--discard-next' || item === '--double-next')) {
+      return undefined;
+    } else if (item ==='--discard-prev') {
+      newArr[i - 1] = undefined;
+      return undefined;
+    } else {
+      return item;
+    }
+  });
+  return newArr;
+}
+
+let a = [ 1,'--discard-prev', 2, 3];
+console.log(transform(a));
