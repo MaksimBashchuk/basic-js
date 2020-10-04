a1858c01559b9d4de1cdedd3d2581350ee6b8ad1
Maksim Bashchuk <MaksimBashchuk@gmail.com>
2020-10-05 01:44:43 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,63 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor (direct = true) {
+    this.val = direct;
+  }
+
+    keyRepeater(message, key) {
+    let newKey = '';
+    let a = message.split(' ').join('');
+    for (let i = 0; i < a.length; i++) {
+      newKey += key[i % key.length];
+    }
+    return newKey;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('THROWN');
+    let newKey = this.keyRepeater(message, key).toLowerCase();
+    let resArr = [];
+    let a = message.split(' ').join('').toLowerCase();
+    for (let i = 0; i < a.length; i++) {
+      if (a.charCodeAt(i) - 97 >= 0 && a.charCodeAt(i) - 97 < 26) {
+        let charIndex = ((a.charCodeAt(i) + newKey.charCodeAt(i) - 194) % 26) + 97;
+        resArr.push(charIndex);
+      } else {
+        resArr.push(a.charCodeAt(i));
+      }
+    }
+    if (!this.val) resArr.reverse();
+    for (let i = 0; i < resArr.length; i++) {
+      if (message[i] === ' ') {
+        resArr.splice(i, 0, message.charCodeAt(i));
+      }
+    }
+    let result = String.fromCharCode(...resArr);
+    return result.toUpperCase();
+  } 
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('THROWN');
+    let newKey = this.keyRepeater(message, key).toLowerCase();
+    let resArr = [];
+    let a = message.split(' ').join('').toLowerCase();
+    for (let i = 0; i < a.length; i++) {
+      if (a.charCodeAt(i) - 97 >= 0 && a.charCodeAt(i) - 97 < 26) {
+        let charIndex = (Math.abs((a.charCodeAt(i) - newKey.charCodeAt(i)) + 26) % 26) + 97;
+        resArr.push(charIndex);
+      } else {
+        resArr.push(a.charCodeAt(i));
+      }
+    }
+    if (!this.val) resArr.reverse();
+    for (let i = 0; i < resArr.length; i++) {
+      if (message[i] === ' ') {
+        resArr.splice(i, 0, message.charCodeAt(i));
+      }
+    }
+    let result = String.fromCharCode(...resArr);
+    return result.toUpperCase();
   }
 }
 
